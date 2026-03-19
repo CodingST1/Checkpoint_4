0017ea2c38447a23187de039309ec7af04c32bf7
Pratyush Baral <156236750+CodingST1@users.noreply.github.com>
2026-03-18 20:36:13 -0400

    Fix route imports and clean up Server.js
    Corrected the import paths for users and appointments routes. Removed duplicate code and ensured proper usage of middleware.

--- Server.js
+++ Server.js
@@ -1,25 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 
-const usersRoutes = require('./routes and users.js/users');
-const appointmentRoutes = require('./routes and users.js/appointments');
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/users', usersRoutes);
-app.use('/api/appointments', appointmentRoutes);
-
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
-});
-const express = require('express');
-const cors = require('cors');
-
-const usersRoutes = require('./routes and users.js/users');
-const appointmentRoutes = require('./routes and users.js/appointments');
+const usersRoutes = require('./routes/users');
+const appointmentRoutes = require('./routes/appointments');
 
 const app = express();
 
